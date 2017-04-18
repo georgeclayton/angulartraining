990e1f22f6fddd72306897a66ce5e69d713cfdb6
James Barry <james.barry93@googlemail.com>
2017-04-18 16:21:38 +0100

    get map working

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -4,17 +4,17 @@ function geoError(error) {
     console.log(error.message);
 }
 
-window.onload = function() {
+window.onload = function () {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(pos){
-            currentPosition = {lat: pos.coords.longitude, lng: pos.coords.latitude}
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            initMap(pos.coords.latitude, pos.coords.longitude)
         }, geoError);
     } else {
         alert('Geolocation is not supported');
     }
 };
 
-function getVenues(latLng){
+function getVenues(latLng) {
     var lat = latLng.lat();
     var lng = latLng.lng();
     var url = "https://apidev.foursquare.com/v2/venues/trending?ll=#{lat},#{lng}&limit=50&radius=15000&#{foursquare_clientID}&#{foursquare_clientSecret}";
@@ -24,23 +24,27 @@ function getVenues(latLng){
         dataType: "jsonp",
         url: url,
         context: document.body,
-        success: function(response){
+        success: function (response) {
             console.log(JSON.stringify(response));
         }
     });
 }
 
-function initMap() {
+function initMap(lat, lng) {
+    var latLng = new google.maps.LatLng(lat, lng);
+
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: currentPosition
-    });
-    var marker = new google.maps.Marker({
-        position: currentPosition,
-        map: map
+        zoom: 15,
+        center: latLng,
     });
 
-    map.addListener('center_changed', function() {
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: "Your location"
+    });
+
+    map.addListener('center_changed', function () {
         // 3 seconds after the center of the map has changed, pan back to the
         // marker.
 //                      window.setTimeout(function() {
