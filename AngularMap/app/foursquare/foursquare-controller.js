dd795a4a19e227532baaaedf1a8d2e0797bc87d9
Haiming Zhao <hm.mira.z@gmail.com>
2017-04-20 14:07:29 +0100

    add get foursquare data

--- AngularMap/app/foursquare/foursquare-controller.js
+++ AngularMap/app/foursquare/foursquare-controller.js
@@ -1,7 +1,27 @@
 'use strict';
-var injectParams = ['$scope'];
+var injectParams = ['$scope', '$http', '$q'];
 
-var foursquareController = function ($scope) {
+var foursquareController = function ($scope, $http, $q) {
+    $scope.lat = 41.878114;
+    $scope.lng = -87.629798;
+
+    var url = 'https://api.foursquare.com/v2/venues/search?v=20161016&ll='+$scope.lat+'%2C%20'+$scope.lng+'&query=coffee&intent=checkin&client_id=UFR43GPU2M23AB3BJNRMNZVWMS15PXHY3KGBSIUHZDXR3RQA&client_secret=QBGT44DY10K5U55VE5WEBW5O00E4QFTSEJWAJBU2JFIOW4SS';
+
+    // function loadFoursquare(){
+        getData().then(displayData).catch(displayError());
+    // }
+
+    function getData(){
+        return $http.get(url);
+    }
+
+    function displayData(response){
+        $scope.data = response.data;
+    }
+
+    function displayError(error){
+        $scope.errorMessage = error;
+    }
 
 };
 foursquareController.$inject = injectParams;
--- AngularMap/app/foursquare/foursquare-service.js
+++ AngularMap/app/foursquare/foursquare-service.js
@@ -2,10 +2,22 @@
 
 var injectParams = ['$http', '$q'];
 
-var foursquareService = function ($http, $q) {
-
+var foursquareFactory = function ($http, $q) {
+    // var data = [];
+    // var url = 'https://api.foursquare.com/v2/venues/search?v=20161016&ll=41.878114%2C%20-87.629798&query=coffee&intent=checkin&client_id=UFR43GPU2M23AB3BJNRMNZVWMS15PXHY3KGBSIUHZDXR3RQA&client_secret=QBGT44DY10K5U55VE5WEBW5O00E4QFTSEJWAJBU2JFIOW4SS';
+    //
+    // $http({method: 'GET', url: url}).then(function (response) {
+    //     data = response.data;
+    // });
+    //
+    //
+    //
+    //
+    // return {
+    //     data: getData
+    // };
 };
 
-foursquareService.$inject = injectParams;
+foursquareFactory.$inject = injectParams;
 
-angular.module('myApp.foursquare').factory('foursquareService', foursquareService);
+angular.module('myApp.foursquare').factory('foursquareFactory', foursquareFactory);
