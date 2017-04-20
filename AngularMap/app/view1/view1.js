783a7bbaaf78e212ba9021feea893fde36cbbfd5
Haiming Zhao <hm.mira.z@gmail.com>
2017-04-20 09:43:51 +0100

    add angular with map component

---
+++ AngularMap/app/view1/view1.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute', 'ngMap'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', [$scope, function($scope) {
+    NgMap.getMap().then(function(map) {
+        console.log(map.getCenter());
+        console.log('markers', map.markers);
+        console.log('shapes', map.shapes);
+    });
+}]);
