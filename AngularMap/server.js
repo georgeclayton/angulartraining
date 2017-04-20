04b0c5a64757d3b29da4e24cc6463b40c8d2f143
George <georgewclayton@gmail.com>
2017-04-20 08:27:03 -0400

    Added bootstrap and a lovely nav bar

---
+++ AngularMap/server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Student on 4/20/2017.
+ */
+var express = require('express');
+
+var app = express();
+
+app.listen(8080, function (err, success) {
+    console.log('running');
+});
